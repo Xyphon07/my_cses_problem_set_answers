bb4959a41da3ba5a92ae25990c0da573618ec856
XYPHON07 <en987176@gmail.com>
2026-08-17 15:07:53 +0530

    initial answer for missing_number

---
+++ Introductory Problems/missing_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int insertion_sort(int n[int],num){
+for(int i=0;i<=num-1;i++){
+
+}
+}
+int main()
+{
+int num;
+scanf(" %d",&num);
+int num1[num];
+for(int i=0;i<=num-2;i++){
+scanf(" %d",&num1[i]);
+}
+
+int temp;
+for(int i=0;i<=num-1;i++){
+for(int j=0;j<=num-1;j++){
+if(i!=num1[j]){temp=i;}
+else break;
+}}
+printf("\n %d ",temp);
+}
